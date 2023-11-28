bbbb03af8fa0a9f75d2593e4bb48b8170afe3d36
wangjiansheng <wangjiansheng@quansantai>
2023-11-28 14:42:53 +0800

    update 7.函数 & add 8.类

--- python编程：从入门到实践/7.函数.py
+++ python编程：从入门到实践/7.函数.py
@@ -44,17 +44,54 @@ def move(a , b):
 # print(b)
 
 ## 禁止函数修改列表
-### 通过切片创建副本
-move(a[:], b[:])
-print(a)
-print(b)
+### 通过切片创建副本，将副本作为参数传递
+# move(a[:], b[:])
+# print(a)
+# print(b)
 
 
 ## 传递任意数量的参数
-def make_pizza(*toppings):
-	print(toppings)
-make_pizza('pepperoni')
-make_pizza('mushroom','pepperoni')
+# def make_pizza(*toppings):
+# 	print(toppings)
+# make_pizza('pepperoni')
+# make_pizza('mushroom','pepperoni')
 ### *toppongs 创建空元组，调用函数将参数封装到元组中
 
-## 任意数量的关键字形参
+
+## 任意数量的关键字实参
+### 形参`**user_info`创建一个名为user_info的空字典，并将收到的键值对封装到字典中
+# def build_profile(first, last, **user_info):
+# 	profile = {}
+# 	profile['first'] = first
+# 	profile['last'] = last
+# 	for key,value in user_info.items():
+# 		profile[key] = value
+# 	return profile
+
+# user_profile = build_profile('albert', 'einstein',
+# 	localtion='princeton', field='physics')
+# print(user_profile)
+
+
+
+## 导入整个模块
+### 模块是`.py`文件
+# import pizza
+# pizza.make_pizza(16, 'pepperoni')
+
+## 导入特定函数
+### 语法 `from module_name import function_0, function_1, function_2`
+# from pizza import make_pizza
+# make_pizza(18, 'mushroom')
+
+### 使用`as`给函数指定别名
+#### 函数指定别名 `from module_name import function_name as fn`
+#### 模块指定别名 `import module_name as mn`
+# from pizza import make_pizza as mp
+# mp(18, 'mushroom')
+
+### 导入模块所有函数
+#### `from pizza import *`
+
+## 给形参指定默认值时，等号两边不要有空格：
+## def function_name(parameter_0, parameter_1='default value')
---
+++ python编程：从入门到实践/8.类.py
@@ -0,0 +1,13 @@
+# 8.类
+
+## 创建类
+class dog():
+	def _init_(self, name, age):
+		self.name = name
+		self.age = age
+	def sit(self)
+		print(self.name.title() + "is now sitting")
+	def roll_over(self)
+		print(self.name.title() + " rolled over")
+
+### `_init_`
